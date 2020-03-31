eff6930ac853c90173420c415b04af1a12063919
Martin Fabián <martin.fabian@centrum.cz>
2020-03-31 22:51:09 +0200

    connect with backend for get one and all recipes

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,8 +1,25 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RecipeService} from '../receipts/recipe-service/recipe.service';
+import {RecipeEntity} from '../receipts/entity/recipe.entity';
 
 @Component({
   selector: 'app-home-page',
-  templateUrl: './home-page.component.html'
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+
+  public recipes: RecipeEntity[];
+
+  constructor(private recipeService: RecipeService) {
+  }
+
+  ngOnInit(): void {
+    this.recipeService.getAllRecipes().subscribe(recipes => {
+      this.recipes = recipes;
+      console.log('returned from backend ' + this.recipes);
+    }, error => console.error(error), () => {
+      console.log('success');
+    });
+  }
 }
--- src/app/components/receipts/recipe-service/recipe.service.ts
+++ src/app/components/receipts/recipe-service/recipe.service.ts
@@ -1,13 +1,23 @@
 import {Injectable} from '@angular/core';
 import {RecipeEntity} from '../entity/recipe.entity';
 import {Observable, of} from 'rxjs';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {RouterConstants} from '../../../constants/router.constants';
+import {catchError, tap} from 'rxjs/operators';
+import {error} from '@angular/compiler/src/util';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
+
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: 'Basic ' + btoa('user:password')
+    })
+  };
 
   recipes: RecipeEntity[] = [{
     id: 1,
@@ -49,20 +59,32 @@ export class RecipeService {
     return of(this.recipes);
   }
 
-  getAllRecipes(): RecipeEntity[] {
-    return [];
+  getAllRecipes(): Observable<RecipeEntity[]> {
+    return this.http.get<RecipeEntity[]>('http://localhost:8080/recipes/list').pipe(
+      tap(_ => console.log('fetched from backend')),
+      catchError(() => error('error fetching data from backend')));
   }
+
 
   addRecipe(recipe: RecipeEntity) {
     return this.http.post(RouterConstants.BASE_URL, recipe);
   }
 
-  getSelectedRecipes(): RecipeEntity {
-    return null;
-  }
-
   getSelectedRecipe(id: number) {
     console.log(this.recipes[id]);
     return this.recipes[id];
+  }
+
+  getRecipeById(id: number): Observable<RecipeEntity> {
+    return this.http.get<RecipeEntity>(`http://localhost:8080/recipes/${id}`).pipe(
+      tap(_ => console.log('fetched one recipe from backend')),
+      catchError(() => error('error fetching data from backend')));
+  }
+
+
+  getRecipeListByUsername(username: string): Observable<RecipeEntity[]> {
+    return this.http.get<RecipeEntity[]>(`http://localhost:8080/recipes/list/${username}`).pipe(
+      tap(_ => console.log('fetched recipe list from backend')),
+      catchError(() => error('error fetching data from backend')));
   }
 }
